9d32a60c80dc63584a831edef34230bbd8e1a6f7
Katherina <kcortes@ucsd.edu>
2022-03-02 10:18:01 -0700

    Initialize

---
+++ README.md
@@ -0,0 +1,17 @@
+# Assembling the Genome
+
+## Goal
+ 
+## Description
+
+## Installation
+
+## Usage
+
+#### Python Usage
+
+#### Command Line Usage
+
+## Input
+
+## Output
--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-import argparse
+# Author: Katherina Cortes
+# Date: Feburary 25, 2022
+# Purpose:
+
+import argparse, random, time
+import graphCreation, graphTraversal
+
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('')
+
+
+def main():
+
+    return
+
+
+main()
 
